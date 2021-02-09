3cf51cea5d0dd4d9bd6bd6f803e197d51c6a7443
Belen <36194885+BelenZapata85@users.noreply.github.com>
2021-02-10 10:19:57 +1100

    added minutes walking/cycling

--- Melbourne_model.R
+++ Melbourne_model.R
@@ -29,22 +29,22 @@ source("Scripts/data_prep/population_prep.R")
 scenarioLocation <- "./scenarios"
 scenarioTripsLocation <- "./scenarios/scenarioTrips"
 outputLocation <- "C:/dot-hia/output/melbourne-outputs-raw" #"/home/alan/DATA/dot-hia/melbourne-outputs-raw"
-combinedLocation <-  "C:/dot-hia/output//melbourne-outputs-combined" # "/home/alan/DATA/dot-hia/melbourne-outputs-combined"
+combinedLocation <-  "C:/dot-hia/output/melbourne-outputs-combined" # "/home/alan/DATA/dot-hia/melbourne-outputs-combined"
 combinedLocationMMETS <-  "C:/dot-hia/output/melbourne-outputs-combined-mmets" #"/home/alan/DATA/dot-hia/melbourne-outputs-combined-mmets"
 summarisedLocation <-  "C:/dot-hia/output/melbourne-outputs-summarised" #"/home/alan/DATA/dot-hia/melbourne-outputs-summarised"
-finalLocation <- "./melbourne-outputs"
+finalLocation <- "C:/dot-hia/output/melbourne-outputs"
 
 ### Scenarios 
 scenarios_Melb <- read.csv("scenarios_for_melbourne.csv",as.is=T,fileEncoding="UTF-8-BOM") %>%
   mutate(scenario_location=paste0(scenarioLocation,"/",scenario,".csv")) %>%
   mutate(trips_location=paste0(scenarioTripsLocation,"/",scenario,".csv")) %>%
   mutate(output_location=paste0(outputLocation,"/",scenario))
-  
+
 
 ### Run model
 print(paste0("iterating through ",nrow(scenarios_Melb)," scenarios at ",Sys.time()))
 for (i in 1:nrow(scenarios_Melb)){
-
+  
   number_cores <- max(1,floor(as.integer(detectCores())*0.8))
   cl <- makeCluster(number_cores)
   cat(paste0("About to start processing results in parallel, using ",number_cores," cores\n"))
@@ -69,11 +69,10 @@ for (i in 1:nrow(scenarios_Melb)){
   cat(paste0("\n scenario ",i,"/",nrow(scenarios_Melb)," complete at ",Sys.time(),"\n"))
 }
 
-####### TO HERE (5/02, CHECK ISSUES WITH SAVING RESULTS)
+
 #### Save combined outputs (for all scenarios and iterations)
 ##### Create directories if not created
 dir.create(combinedLocation, recursive=TRUE, showWarnings=FALSE)
-dir.create(combinedLocationMMETS, recursive=TRUE, showWarnings=FALSE)
 
 print(paste0("merging ",nrow(scenarios_Melb)," scenario outputs into single file at ",Sys.time()))
 
@@ -86,25 +85,17 @@ combineOutputs <- function(inputDirectory,outputFile) {
   saveRDS(output_df, file=outputFile)
 }
 
+### Combine outputs and save (outputs raw here "C:/dot-hia/output/melbourne-outputs-raw" get combined to here "C:/dot-hia/output/melbourne-outputs-combined")
+
 for (i in 1:nrow(scenarios_Melb)){
   combineOutputs(paste0(scenarios_Melb[i,]$output_location,'/output_df'),
                  paste0(combinedLocation,"/",scenarios_Melb[i,]$scenario,".rds"))
   cat(paste0("\n combined scenario ",i,"/",nrow(scenarios_Melb)," complete at ",Sys.time(),"\n"))
 }
 
-for (i in 1:nrow(scenarios_Melb)){
-  combineOutputs(paste0(scenarios_Melb[i,]$output_location,'/mmets'),
-                 paste0(combinedLocationMMETS,"/",scenarios_Melb[i,]$scenario,".rds"))
-  cat(paste0("\n combined mmets scenario ",i,"/",nrow(scenarios_Melb)," complete at ",Sys.time(),"\n"))
-}
 
-for (i in 1:nrow(scenarios_Melb)){
-  combineOutputs(paste0(scenarios_Melb[i,]$output_location,'/mmets'),
-                 paste0(combinedLocationMMETS,"/",scenarios_Melb[i,]$scenario,".rds"))
-  cat(paste0("\n combined mmets scenario ",i,"/",nrow(scenarios_Melb)," complete at ",Sys.time(),"\n"))
-}
-
-###### ALAN, WHAT IS THIS DOING???
+ ### Summarize outputs and save for each scenario ("C:/dot-hia/output/melbourne-outputs-summarised")
+#### Saved items include: output_df_aggregate, output_life_expectancy_change, output_life_year_change, output_disease_change
 
 print(paste0("summarising ",nrow(scenarios_Melb)," scenario outputs at ",Sys.time()))
 for (i in 1:nrow(scenarios_Melb)){
@@ -115,27 +106,86 @@ for (i in 1:nrow(scenarios_Melb)){
   cat(paste0("\n combined scenario ",i,"/",nrow(scenarios_Melb)," complete at ",Sys.time(),"\n"))
 }
 
-print(paste0("summarising mmets ",nrow(scenarios_Melb)," scenario outputs at ",Sys.time()))
-for (i in 1:nrow(scenarios_Melb)){
-  output_df <- readRDS(paste0(combinedLocationMMETS,"/",scenarios_Melb[i,]$scenario,".rds"))
-  summariseMMETS(scenario_location=
-                     paste0(summarisedLocation,"/",scenarios_Melb[i,]$scenario),
-                   output_df)
-  cat(paste0("\n combined mmets scenario ",i,"/",nrow(scenarios_Melb)," complete at ",Sys.time(),"\n"))
-}
 
+### Save to melbourne-outputs
 print(paste0("summarising transport modes ",nrow(scenarios_Melb)," scenario outputs at ",Sys.time()))
 scenarioTrips<-NULL
 for (i in 1:nrow(scenarios_Melb)){
   scenarioTripsCurrent<-summariseTransport(scenarios_Melb[i,]$trips_location,
                                            scenarios_Melb[i,]$scenario)
   scenarioTrips<-bind_rows(scenarioTrips,scenarioTripsCurrent)
-  cat(paste0("\n combined mmets scenario ",i,"/",nrow(scenarios_Melb)," complete at ",Sys.time(),"\n"))
+  cat(paste0("\n combined transport scenario ",i,"/",nrow(scenarios_Melb)," complete at ",Sys.time(),"\n"))
 }
+
+## Create weighted modes (modes of transport are from survey data, to represent population we need to weight)
+scenario_trips_weighted <- list()
+index <- 1
+for (a in unique(scenarioTrips$age)){
+  for (s in unique(scenarioTrips$sex)){
+    for (sc in unique(scenarioTrips$scenario)){
+      for (scen in unique(scenarioTrips$scen)) {
+        scenario_trips_weighted[[index]] <-   scenarioTrips  %>%
+          dplyr::filter(age==a, sex==s, scenario==sc, scen==scen) %>%
+          srvyr::as_survey_design(weights = participant_wt) %>% 
+          group_by(mode) %>%
+          dplyr::summarize(prop= srvyr::survey_mean(), 
+                           trips = srvyr::survey_total(),
+                           surveyed = srvyr::unweighted(dplyr::n())) %>%
+          dplyr::mutate(age=a, 
+                        sex=s,
+                        scenario=sc,
+                        scen=scen) %>%
+          dplyr::select(age, sex, scenario, scen, prop, trips, surveyed, mode)
+        index <- index +1}}}}
+
+scenarioTrips<- bind_rows(scenario_trips_weighted) 
 saveRDS(scenarioTrips,paste0(finalLocation,"/output_transport_modes.rds"))
 
+## Create PA file and weighed stats
+### Combine PA (from ./scenarios file)
 
-# combine scenarios into single files
+#### Minutes per week walking and cycling
+#### Create combined file (and add all age groups and sex)
+PA_files<-list.files(scenarioLocation,pattern="*.csv",full.names=T)
+data<-lapply(PA_files,read.csv,header=T) %>%
+  bind_rows() %>%
+  dplyr::select(participant_wt,age,sex,ses,mod_total_hr:scen) %>%
+  mutate(agegroup= case_when(
+    age>=15 & age<=19 ~'15-19',
+    age>=20 & age<=39 ~'20-39',
+    age>=40 & age<=64 ~'40-64',
+    age>=65           ~'65plus')) %>%
+  dplyr::select(participant_wt,age=agegroup,sex,ses,mod_total_hr:scen)
+dataAll <- bind_rows(
+  data,
+  data%>%mutate(age='all'),
+  data%>%mutate(sex='all'),
+  data%>%mutate(age='all',sex='all'))
+
+### Create weighted stats
+PA_weighted  <- dataAll %>% 
+  srvyr::as_survey_design(weights = participant_wt)%>%
+  group_by(age, sex, scen) %>%
+  dplyr::summarize(walk_base= srvyr::survey_mean(time_base_walking*60), 
+                   walk_scen = srvyr::survey_mean(time_scen_walking*60),
+                   cycle_base= srvyr::survey_mean(time_base_bicycle*60), 
+                   cycle_scen = srvyr::survey_mean(time_scen_bicycle*60))
+
+saveRDS(PA_weighted, file=paste0(finalLocation, "/PAall.rds"))
+
+### Meets guidelines
+PA_weighted  <- dataAll %>% 
+  dplyr::mutate(meets_pa_base=ifelse((time_base_walking + walk_rc + time_base_bicycle*2 + mod_leis_hr + vig_leis_hr*2)*60 >= 150, 1, 0), 
+                meets_pa_scen= ifelse((time_scen_walking + walk_rc + time_scen_bicycle*2 + mod_leis_hr + vig_leis_hr*2)*60>= 150, 1, 0))
+
+PA_guide_weighted  <- PA_weighted %>% 
+  srvyr::as_survey_design(weights = participant_wt)%>%
+  group_by(age, sex, scen) %>%
+  dplyr::summarize(meets_base= srvyr::survey_mean(meets_pa_base, na.rm = T), 
+                   meets_scen = srvyr::survey_mean(meets_pa_scen, na.rm = T))
+saveRDS(PA_guide_weighted, file=paste0(finalLocation, "/PAallGuide.rds"))
+
+# combine scenarios into single files 
 
 combineScenarios <- function(summarisedLocation,name) {
   scenario_names<-data.frame(scen=list.files(summarisedLocation)) %>%
@@ -159,122 +209,14 @@ output_life_expectancy_change <- combineScenarios(summarisedLocation,name="outpu
   rename(age=age_group_final,sex=Gender)
 output_life_years_change <- combineScenarios(summarisedLocation,name="output_life_years_change.csv") %>%
   rename(age=age_group_final,sex=Gender)
-output_mmets <- combineScenarios(summarisedLocation,name="output_mmets.csv")
-output_mmets_graph <- combineScenarios(summarisedLocation,name="output_mmets_graph.csv")
 
-# in case the directory hasn't been made yet
+# in case the directory hasn't been made yet (save here ".melbourne_outputs")
 dir.create(finalLocation, recursive=TRUE, showWarnings=FALSE)
 saveRDS(output_df_agg_all,paste0(finalLocation,"/output_df_agg.rds"))
 saveRDS(output_diseases_change,paste0(finalLocation,"/output_diseases_change.rds"))
 saveRDS(output_life_expectancy_change,paste0(finalLocation,"/output_life_expectancy_change.rds"))
 saveRDS(output_life_years_change,paste0(finalLocation,"/output_life_years_change.rds"))
-saveRDS(output_mmets,paste0(finalLocation,"/output_mmets.rds"))
-saveRDS(output_mmets_graph,paste0(finalLocation,"/output_mmets_graph.rds"))
 
-
-
-###### BELOW CAN BE DELETED
-
-#### Graphs for changes in diseases over time 
-
-### Incidence
-
-data_f <- dplyr::filter(outputs[['output_df_agg_sex']], sex == "female") %>% dplyr::select("sex", "year", contains("diff"))
-data_m <- dplyr::filter(outputs[['output_df_agg_sex']], sex == "male") %>% dplyr::select("sex", "year", contains("diff"))
-data_t <- dplyr::filter(outputs[['output_df_agg_all']]) %>% dplyr::select("year", contains("diff")) %>%
-  mutate(sex ="total")
-
-data <- bind_rows(data_f, data_m, data_t)
-
-plot <- plot <- data %>%
-  ggplot(aes(x = year, y = inc_num_diff_brsc)) +
-  geom_smooth(method = "loess") +
-  geom_smooth(aes(y = inc_num_diff_carc, method = "loess", color="Colon cancer")) +
-  geom_smooth(aes(y = inc_num_diff_dmt2, method = "loess", color="Type 2 diabetes")) +
-  geom_smooth(aes(y = inc_num_diff_tbalc, method = "loess", color="Lung cancer")) +
-  geom_smooth(aes(y = inc_num_diff_utrc, method = "loess", color="Uterine cancer")) +
-  geom_smooth(aes(y = inc_num_diff_ishd, method = "loess", color="Ischemic heart disease")) +
-  geom_smooth(aes(y = inc_num_diff_strk, method = "loess", color="Stroke")) + 
-  labs(x = "Simulation year",
-       title = paste("Changes in disease incidence over time"),
-       y = "Numbers") +
-  labs(color="") +
-  theme(plot.title = element_text(hjust = 0.5, size = 12,face="bold"),
-        axis.text=element_text(size=10),
-        axis.title=element_text(size=10)) +
-  theme_classic() +
-  geom_hline(yintercept=0, linetype='dashed', color = 'black')+
-  facet_wrap(. ~ sex) + 
-  theme(
-    strip.background = element_blank() ) +
-  scale_fill_brewer(name = "Sex") +
-  theme(legend.position = "bottom",
-        legend.text = element_text(colour = "black", size = 8),
-        legend.key = element_blank(),
-        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave(plot, file=paste0("./output/diseases/incidence.png"), width = 14, height = 10, units = "cm")
-
-
-### Deaths
-
-plot <- plot <- data %>%
-  ggplot(aes(x = year, y = mx_num_diff_brsc)) +
-  geom_smooth(method = "loess") +
-  geom_smooth(aes(y = mx_num_diff_carc, method = "loess", color="Colon cancer")) +
-  geom_smooth(aes(y = mx_num_diff_dmt2, method = "loess", color="Type 2 diabetes")) +
-  geom_smooth(aes(y = mx_num_diff_tbalc, method = "loess", color="Lung cancer")) +
-  geom_smooth(aes(y = mx_num_diff_utrc, method = "loess", color="Uterine cancer")) +
-  geom_smooth(aes(y = mx_num_diff_ishd, method = "loess", color="Ischemic heart disease")) +
-  geom_smooth(aes(y = mx_num_diff_strk, method = "loess", color="Stroke")) + 
-  labs(x = "Simulation year",
-       title = paste("Changes in disease mortality over time"),
-       y = "Numbers") +
-  labs(color="") +
-  theme(plot.title = element_text(hjust = 0.5, size = 12,face="bold"),
-        axis.text=element_text(size=10),
-        axis.title=element_text(size=10)) +
-  theme_classic() +
-  geom_hline(yintercept=0, linetype='dashed', color = 'black')+
-  facet_wrap(. ~ sex) + 
-  theme(
-    strip.background = element_blank() ) +
-  scale_fill_brewer(name = "Sex") +
-  theme(legend.position = "bottom",
-        legend.text = element_text(colour = "black", size = 8),
-        legend.key = element_blank(),
-        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave(plot, file=paste0("./output/diseases/deaths.png"), width = 14, height = 10, units = "cm")
-
-
-#### Graph for changes in life years 
-
-data_f <- dplyr::filter(outputs[['output_df_agg_sex']], sex == "female") %>% dplyr::select("sex", "year", "Lx_diff", "Lwx_diff")
-data_m <- dplyr::filter(outputs[['output_df_agg_sex']], sex == "male") %>% dplyr::select("sex", "year", "Lx_diff", "Lwx_diff")
-data_t <-  dplyr::filter(outputs[['output_df_agg_all']]) %>% dplyr::select("year", "Lx_diff", "Lwx_diff")       
-
-plot <- data_t %>%
-  ggplot(aes(x = year, y = Lx_diff)) +
-  geom_smooth(method = "loess", aes(color= "Life years total")) +
-  geom_smooth(data = data_t, aes(y = Lwx_diff, method = "loess",  color= "HALYs total")) +
-  geom_smooth(data = data_f, aes(y = Lx_diff,  method = "loess",  color= "Life years female")) +
-  geom_smooth(data = data_f, aes(y = Lwx_diff,  method = "loess",  color= "HALYs female")) +
-  geom_smooth(data = data_m, aes(y = Lx_diff,  method = "loess",  color= "Life years male")) +
-  geom_smooth(data = data_m, aes(y = Lwx_diff,  method = "loess",  color= "HALYs male")) +
-  labs(x = "Simulation year",
-       title = paste("Difference life years and health-adjusted life years"),
-       y = "Numbers") +
-  labs(color="") +
-  theme(plot.title = element_text(hjust = 0.5, size = 12,face="bold"),
-        axis.text=element_text(size=10),
-        axis.title=element_text(size=10)) +
-  theme(legend.position = "right",
-        legend.title = element_blank(),
-        legend.text = element_text(colour = "black", size = 10),
-        legend.key = element_blank(),
-        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
-  theme_classic() +
-  geom_hline(yintercept=0, linetype='dashed', color = 'black')
-ggsave(plot, file=paste0("./output/LifeYears/lifeyears.png"), width = 14, height = 10, units = "cm")
 
 
 
