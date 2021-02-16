b5ee7102c2828142103dbb3842aa1b7666ee3ad8
Belen <36194885+BelenZapata85@users.noreply.github.com>
2021-02-16 11:11:39 +1100

    run 10 iterations

--- Melbourne_model.R
+++ Melbourne_model.R
@@ -39,7 +39,7 @@ for (i in 1:nrow(scenarios_Melb)){
   cl <- makeCluster(number_cores)
   cat(paste0("About to start processing results in parallel, using ",number_cores," cores\n"))
   persons_matched=read.csv(scenarios_Melb[i,]$scenario_location,as.is=T, fileEncoding="UTF-8-BOM")
-  seeds<-1:1000
+  seeds<-1:10
   registerDoParallel(cl)
   start_time = Sys.time()
   results <- foreach::foreach(seed_current=seeds,
@@ -110,7 +110,7 @@ for (i in 1:nrow(scenarios_Melb)){
 
 
 ### Two options for transport trips, weighted and unweigthed, trying to find out issue with walking changing when scenarios does not change
-scenario_trips_unweighted <- list()
+scenario_trips_weighted <- list()
 iage <- c(unique(scenarioTrips$age))
 isex <- c(unique(scenarioTrips$sex))
 iscenario <- c(unique(scenarioTrips$scenario))
@@ -123,48 +123,23 @@ for (a in iage){
         data <- scenarioTrips %>%
         dplyr::filter(age==a, sex==s, scenario==sc, scen==scena)
         mode_share <- data %>%
-          group_by(mode) %>%
-          summarise(n = n()) %>%
-          mutate(prop = n / sum(n)) %>%
+          srvyr::as_survey_design(weights = participant_wt) %>%
+                    group_by(mode) %>%
+                    dplyr::summarize(prop= srvyr::survey_mean(na.rm = T),
+                                     trips = srvyr::survey_total(na.rm = T),
+                                     surveyed = srvyr::unweighted(dplyr::n())) %>%
           ungroup() %>%
           dplyr::mutate(age=a,
                         sex=s,
                         scenario=sc,
                         scen=scena) %>%
           dplyr::select(age, sex, scenario, scen, prop, mode)
-        scenario_trips_unweighted[[index]] <- mode_share
+        scenario_trips_weighted[[index]] <- mode_share
         index <- index +1}}}}
 
-scenarioTrips<- bind_rows(scenario_trips_unweighted)
+scenarioTrips<- bind_rows(scenario_trips_weighted)
 saveRDS(scenarioTrips,paste0(finalLocation,"/output_transport_modes.rds"))
 
-#### Belen to adjust as ABOVE
-# ## Create weighted modes (modes of transport are from survey data, to represent population we need to weight)
-# scenario_trips_weighted <- list()
-# index <- 1
-# for (a in unique(scenarioTrips$age)){
-#   for (s in unique(scenarioTrips$sex)){
-#     for (sc in unique(scenarioTrips$scenario)){
-#       for (scen in unique(scenarioTrips$scen)) {
-#         scenario_trips_weighted[[index]] <- scenarioTrips %>%
-#           dplyr::filter(age==a, sex==s, scenario==sc, scen==scen) %>%
-#           srvyr::as_survey_design(weights = participant_wt) %>%
-#           group_by(mode) %>%
-#           dplyr::summarize(prop= srvyr::survey_mean(na.rm = T),
-#                            trips = srvyr::survey_total(na.rm = T),
-#                            surveyed = srvyr::unweighted(dplyr::n(na.rm = T))) %>%
-#           dplyr::mutate(age=a,
-#                         sex=s,
-#                         scenario=sc,
-#                         scen=scen) %>%
-#           dplyr::select(age, sex, scenario, scen, prop, trips, surveyed, mode)
-#         index <- index +1}}}}
-# 
-# scenarioTrips<- bind_rows(scenario_trips_weighted)
-# saveRDS(scenarioTrips,paste0(finalLocation,"/output_transport_modes.rds"))
-# 
-# ## Create PA file and weighed stats
-# ### Combine PA (from ./scenarios file)
 # 
 #### Minutes per week walking and cycling
 #### Create combined file (and add all age groups and sex)
